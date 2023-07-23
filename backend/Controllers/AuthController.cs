1b821b51ed3274c8e35bdf77205a5acfe470c74b
VLola <valiklolanew@gmail.com>
2023-07-23 22:57:40 +0300

    added jwt token

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -1,6 +1,11 @@
 ﻿using backend.Models;
 using backend.Services;
+using Firebase.Database;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace backend.Controllers
 {
@@ -8,7 +13,7 @@ namespace backend.Controllers
     [Route("[controller]")]
     public class AuthController : Controller
     {
-        [HttpPost("Login")]
+        [HttpGet("Login")]
         public async Task<ActionResult> Login(string email, string password)
         {
             if (password.Length < 6) return BadRequest("Password less than 6 characters!");
@@ -18,7 +23,8 @@ namespace backend.Controllers
             if (result?.Object == null) return Conflict("User does not exist");
             User user = result.Object;
             if (!BCrypt.Net.BCrypt.Verify(password, user.Password)) return Conflict("Wrong data");
-            return Ok("Ok");
+
+            return Ok(GetJWTToken(result));
         }
         [HttpPost("Registration")]
         public async Task<ActionResult> Registration(string email, string password)
@@ -33,7 +39,27 @@ namespace backend.Controllers
             user.Password = BCrypt.Net.BCrypt.HashPassword(password);
             var result = await UserService.Add(user);
             if (result.Object == null) return Conflict("Failed registration");
-            else return Ok("Ok");
+
+            return Ok(GetJWTToken(result));
+        }
+
+        private static JWTTokenResponse GetJWTToken(FirebaseObject<User> user)
+        {
+            List<Claim> claims = new();
+            claims.Add(new Claim(ClaimTypes.PrimarySid, user.Key));
+            claims.Add(new Claim(ClaimTypes.Email, user.Object.Email));
+
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ConfigurationManager.AppSetting["JWT:Secret"]));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+            var tokeOptions = new JwtSecurityToken(
+                issuer: ConfigurationManager.AppSetting["JWT:ValidIssuer"],
+                audience: ConfigurationManager.AppSetting["JWT:ValidAudience"],
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: signinCredentials
+            );
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+            return new JWTTokenResponse { Token = tokenString };
         }
     }
 }
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ConfigurationManager = backend.ConfigurationManager;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +12,37 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(opt => {
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = ConfigurationManager.AppSetting["JWT:ValidIssuer"],
+            ValidAudience = ConfigurationManager.AppSetting["JWT:ValidAudience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ConfigurationManager.AppSetting["JWT:Secret"]))
+        };
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                if (context.Request.Headers.ContainsKey("sec-websocket-protocol") && context.HttpContext.WebSockets.IsWebSocketRequest)
+                {
+                    var token = context.Request.Headers["sec-websocket-protocol"].ToString();
+                    // token arrives as string = "client, xxxxxxxxxxxxxxxxxxxxx"
+                    context.Token = token.Substring(token.IndexOf(',') + 1).Trim();
+                    context.Request.Headers["sec-websocket-protocol"] = "client";
+                }
+                return Task.CompletedTask;
+            }
+        };
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,8 +54,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
+app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
 app.Run();
