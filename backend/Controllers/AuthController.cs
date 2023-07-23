8357ad824c9a021de2fabeed0d096c0a62f9c933
VLola <valiklolanew@gmail.com>
2023-07-23 22:25:51 +0300

    added user model, user service, firebase database, auth controller

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using backend.Models;
+using backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        [HttpPost("Login")]
+        public async Task<ActionResult> Login(string email, string password)
+        {
+            if (password.Length < 6) return BadRequest("Password less than 6 characters!");
+            var addr = new System.Net.Mail.MailAddress(email);
+            if (addr.Address != email) return BadRequest("Email not validate");
+            var result = await UserService.FindUser(email);
+            if (result?.Object == null) return Conflict("User does not exist");
+            User user = result.Object;
+            if (!BCrypt.Net.BCrypt.Verify(password, user.Password)) return Conflict("Wrong data");
+            return Ok("Ok");
+        }
+        [HttpPost("Registration")]
+        public async Task<ActionResult> Registration(string email, string password)
+        {
+            if (password.Length < 6) return BadRequest("Password less than 6 characters!");
+            var addr = new System.Net.Mail.MailAddress(email);
+            if (addr.Address != email) return BadRequest("Email not validate");
+            var find = await UserService.FindUser(email);
+            if (find != null && find.Object != null) return Conflict("User exists");
+            User user = new User();
+            user.Email = email;
+            user.Password = BCrypt.Net.BCrypt.HashPassword(password);
+            var result = await UserService.Add(user);
+            if (result.Object == null) return Conflict("Failed registration");
+            else return Ok("Ok");
+        }
+    }
+}
---
+++ backend/Services/UserService.cs
@@ -0,0 +1,52 @@
+﻿using backend.Models;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace backend.Services
+{
+    public static class UserService
+    {
+        private static string firebaseDatabaseUrl = "https://database-50f39-default-rtdb.europe-west1.firebasedatabase.app/";
+        private static readonly FirebaseClient firebaseClient = new FirebaseClient(firebaseDatabaseUrl);
+
+        public static async Task<FirebaseObject<User>> Add(User user)
+        {
+            return await firebaseClient
+              .Child("Users")
+              .PostAsync(user);
+        }
+        public static async Task<List<KeyValuePair<string, User>>?> GetUsers()
+        {
+            var users = await firebaseClient
+              .Child("Users")
+              .OnceAsync<User>();
+
+            return users?
+              .Select(x => new KeyValuePair<string, User>(x.Key, x.Object))
+              .ToList();
+        }
+        public static async Task UpdateUser(string id, User user)
+        {
+            await firebaseClient
+              .Child("Users")
+              .Child(id)
+              .PutAsync(user);
+        }
+        public static async Task RemoveUser(string id)
+        {
+            await firebaseClient
+              .Child("students")
+              .Child(id)
+              .DeleteAsync();
+        }
+        public static async Task<FirebaseObject<User>?> FindUser(string email)
+        {
+            var users = await firebaseClient
+              .Child("Users")
+              .OnceAsync<User>();
+
+            return users?
+              .FirstOrDefault(user => user.Object.Email == email);
+        }
+    }
+}
