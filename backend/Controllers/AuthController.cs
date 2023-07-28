222495221203e2fc4a17c4908185065b8ef45275
VLola <valiklolanew@gmail.com>
2023-07-28 19:34:06 +0300

    added user sign up

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -3,7 +3,6 @@ using backend.Services;
 using Firebase.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -14,45 +13,6 @@ namespace backend.Controllers
     [Route("[controller]")]
     public class AuthController : Controller
     {
-        [HttpGet("Login")]
-        public async Task<ActionResult> Login(string email, string password)
-        {
-            if (password.Length < 6) return BadRequest("Password less than 6 characters!");
-            var addr = new System.Net.Mail.MailAddress(email);
-            if (addr.Address != email) return BadRequest("Email not validate");
-            var result = await UserService.FindUserByEmailAsync(email);
-            if (result?.Object == null) return Conflict("User does not exist");
-            User user = result.Object;
-            if (!BCrypt.Net.BCrypt.Verify(password, user.Password)) return Conflict("Wrong data");
-
-            user.LastVisit = DateTime.UtcNow;
-            await UserService.UpdateUserAsync(result.Key, user);
-
-            return Ok(GetResponse(result));
-        }
-        [HttpPost("Registration")]
-        public async Task<ActionResult> Registration(string email, string password)
-        {
-            if (password.Length < 6) return BadRequest("Password less than 6 characters!");
-            var addr = new System.Net.Mail.MailAddress(email);
-            if (addr.Address != email) return BadRequest("Email not validate");
-            var find = await UserService.FindUserByEmailAsync(email);
-            if (find != null && find.Object != null) return Conflict("User exists");
-            User user = new User();
-            user.Email = email;
-            user.Role = Role.User;
-            user.Status = Status.Active;
-            user.LastVisit = DateTime.UtcNow;
-            user.Password = BCrypt.Net.BCrypt.HashPassword(password);
-
-            var result = await UserService.AddUserAsync(user);
-            if (result.Object == null) return Conflict("Failed registration");
-
-            user.Id = result.Key;
-            await UserService.UpdateUserAsync(result.Key, user);
-
-            return Ok(GetResponse(result));
-        }
         [HttpGet("SignIn")]
         public async Task<ActionResult> SignIn(string email, string password)
         {
@@ -70,6 +30,16 @@ namespace backend.Controllers
 
                 return Ok(GetResponse(find));
             }
+            else return Conflict("User does not exist");
+        }
+        [HttpPost("SignUp")]
+        public async Task<ActionResult> SignUp(string email, string password)
+        {
+            if (password.Length < 6) return BadRequest("Password less than 6 characters!");
+            var addr = new System.Net.Mail.MailAddress(email);
+            if (addr.Address != email) return BadRequest("Email not validate");
+            var find = await UserService.FindUserByEmailAsync(email);
+            if (find != null && find.Object != null) return Conflict("User exists");
             else
             {
                 User user = new User();
@@ -85,7 +55,7 @@ namespace backend.Controllers
                 user.Id = result.Key;
                 await UserService.UpdateUserAsync(result.Key, user);
 
-                return Ok(GetResponse(result));
+                return Ok("Registration successful");
             }
         }
         private static Response GetResponse(FirebaseObject<User> user)
