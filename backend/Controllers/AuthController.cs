5dfb04369365c711005204eadd6250814a11b9bf
VLola <valiklolanew@gmail.com>
2023-08-02 13:53:30 +0300

    changed SignUp

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using backend.Services;
 using Firebase.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
@@ -33,23 +34,31 @@ namespace backend.Controllers
             else return Conflict("User does not exist");
         }
         [HttpPost("SignUp")]
-        public async Task<ActionResult> SignUp(string email, string password)
-        {
-            email = email.ToLower();
-            if (password.Length < 6) return BadRequest("Password less than 6 characters!");
-            var addr = new System.Net.Mail.MailAddress(email);
-            if (addr.Address != email) return BadRequest("Email not validate");
-            var find = await UserService.FindUserByEmailAsync(email);
+        public async Task<ActionResult> SignUp(User userData)
+        { 
+            if (userData == null ||
+                string.IsNullOrEmpty(userData.Email) || 
+                string.IsNullOrEmpty(userData.Password) || 
+                string.IsNullOrEmpty(userData.FirstName) || 
+                string.IsNullOrEmpty(userData.LastName)) return BadRequest("User data null or empty");
+            
+            userData.Email = userData.Email.ToLower();
+            if (userData.Password.Length < 6) return BadRequest("Password less than 6 characters!");
+            var addr = new System.Net.Mail.MailAddress(userData.Email);
+            if (addr.Address != userData.Email) return BadRequest("Email not validate");
+            var find = await UserService.FindUserByEmailAsync(userData.Email);
             if (find != null && find.Object != null) return Conflict("User exists");
             else
             {
                 User user = new User();
-                user.Email = email;
+                user.Email = userData.Email;
+                user.FirstName = userData.FirstName;
+                user.LastName = userData.LastName;
                 user.Role = Role.User;
                 user.Status = Status.Active;
                 user.LastVisit = DateTime.UtcNow;
                 user.Joined = DateOnly.FromDateTime(DateTime.Now).ToLongDateString();
-                user.Password = BCrypt.Net.BCrypt.HashPassword(password);
+                user.Password = BCrypt.Net.BCrypt.HashPassword(userData.Password);
 
                 var result = await UserService.AddUserAsync(user);
                 if (result.Object == null) return Conflict("Failed registration");
