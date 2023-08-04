baae6a4fffeb44a02b4e79aad3d07799069629d8
VLola <valiklolanew@gmail.com>
2023-08-04 16:24:58 +0300

    fixed bugs

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -15,20 +15,20 @@ namespace backend.Controllers
         [HttpGet("GetUsers")]
         public async Task<ActionResult> GetUsers()
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
-            IEnumerable<UserBase>? users = await UserService.GetUsersAsync(resultValidate.userId);
+            IEnumerable<UserBase>? users = await UserService.GetUsersAsync(resultValidate.user.Id);
             return Ok(users);
         }
         [Authorize]
         [HttpGet("GetFriends")]
         public async Task<ActionResult> GetFriends()
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
-            var result = await UserService.GetFriendsAsync(resultValidate.userId);
+            var result = await UserService.GetFriendsAsync(resultValidate.user.Id);
 
             return Ok(result);
         }
@@ -36,17 +36,17 @@ namespace backend.Controllers
         [HttpPost("AddFriend")]
         public async Task<ActionResult> AddFriend(string id)
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
             User? recipient = await UserService.FindUserByIdAsync(id);
             if (recipient == null) return NotFound("Recipient not found!");
 
-            var result = await UserService.FindFriendAsync(resultValidate.userId, id);
+            var result = await UserService.FindFriendAsync(resultValidate.user.Id, id);
 
             if (result != null) return Conflict("Invitation already sent");
 
-            bool check = await UserService.AddFriendAsync(resultValidate.userId, id);
+            bool check = await UserService.AddFriendAsync(resultValidate.user.Id, id);
             if (!check) return Conflict("Add friend failed");
             else return Ok("Friend invite sent");
         }
@@ -54,13 +54,13 @@ namespace backend.Controllers
         [HttpPost("ConfirmFriend")]
         public async Task<ActionResult> ConfirmFriend(string id)
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
             User? recipient = await UserService.FindUserByIdAsync(id);
             if (recipient == null) return NotFound("Recipient not found!");
 
-            var result = await UserService.ConfirmFriendAsync(resultValidate.userId, id);
+            var result = await UserService.ConfirmFriendAsync(resultValidate.user.Id, id);
 
             if (!result) return Conflict("Confirm friend failed");
             else return Ok("Friend added");
@@ -69,13 +69,13 @@ namespace backend.Controllers
         [HttpDelete("RemoveFriend")]
         public async Task<ActionResult> RemoveFriend(string id)
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
             User? recipient = await UserService.FindUserByIdAsync(id);
             if (recipient == null) return NotFound("Recipient not found!");
 
-            await UserService.RemoveFriendAsync(resultValidate.userId, id);
+            await UserService.RemoveFriendAsync(resultValidate.user.Id, id);
 
             return Ok("Friend removed");
         }
@@ -83,14 +83,14 @@ namespace backend.Controllers
         [HttpPost("AddGroup")]
         public async Task<ActionResult> AddGroup(Group group)
         {
-            Console.WriteLine(JsonConvert.SerializeObject(group));
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
-            group.AdminId = resultValidate.userId;
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
+
+            group.AdminId = resultValidate.user.Id;
             var result = await UserService.AddGroupAsync(group);
             if (result.Object == null) return Conflict("Error");
             group.Id = result.Key;
-            group.Users.Add(resultValidate.userId,true);
+            group.Users.Add(resultValidate.user.Id,true);
             await UserService.UpdateGroupAsync(result.Key, group);
             return Ok("Group added");
         }
@@ -98,8 +98,8 @@ namespace backend.Controllers
         [HttpGet("GetGroups")]
         public async Task<ActionResult> GetGroups()
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
             IEnumerable<Group>? groups = await UserService.GetGroupsAsync();
             return Ok(groups);
@@ -108,11 +108,12 @@ namespace backend.Controllers
         [HttpPost("JoinGroup")]
         public async Task<ActionResult> JoinGroup(string id)
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
+
             Group? group = await UserService.GetGroupByIdAsync(id);
             if (group == null) return NotFound("Group Not Found!");
-            group.Users.Add(resultValidate.userId,group.Audience == Audience.Private? false:true);
+            group.Users.Add(resultValidate.user.Id,group.Audience == Audience.Private? false:true);
             //await Console.Out.WriteLineAsync(group);
             await UserService.UpdateGroupAsync(id, group);
             return Ok("Request has been sent");
@@ -122,47 +123,53 @@ namespace backend.Controllers
         [HttpPost("UpdateUser")]
         public async Task<ActionResult> UpdateUser(User data)
         {
-            Console.WriteLine(JsonConvert.SerializeObject(data));
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
-            
-            var user = await UserService.FindUserByIdAsync(resultValidate.userId);
+            User user = resultValidate.user;
 
-            if (user != null)
+            // Check email
+            if(string.IsNullOrEmpty(data.Email)) return BadRequest("Email null or empty");
+            data.Email = data.Email.ToLower();
+
+            if(user.Email != data.Email)
             {
-                // Update user
+                var addr = new System.Net.Mail.MailAddress(data.Email);
+                if (addr.Address != data.Email) return BadRequest("Email not validate");
+                var find = await UserService.FindUserByEmailAsync(data.Email);
+                if (find != null) return Conflict("Email exist"); 
+            }
+
+            // Update user
+            if(!string.IsNullOrEmpty(data.Born))
+            {
                 DateOnly born = DateOnly.Parse(data.Born.ToString());
-                user.Born = born.ToLongDateString();       
-                user.FirstName = data.FirstName;
-                user.LastName = data.LastName;
-                user.Phone = data.Phone;
-                user.Gender = data.Gender;
-                user.Status = Status.Active;                                                                        
-                user.FamilyStatus = data.FamilyStatus;
-                user.AboutMe = data.AboutMe;
-                user.Location = data.Location;
-                user.Work = data.Work;
-                await UserService.UpdateUserAsync(resultValidate.userId, user);
-                return Ok("User is Updated");
+                user.Born = born.ToLongDateString();
             }
-            else
-            {
-                return NotFound("User Not Found");
-            }
-
+            user.Email = data.Email;
+            user.FirstName = data.FirstName;
+            user.LastName = data.LastName;
+            user.Phone = data.Phone;
+            user.Gender = data.Gender;
+            user.Status = Status.Active;
+            user.FamilyStatus = data.FamilyStatus;
+            user.AboutMe = data.AboutMe;
+            user.Location = data.Location;
+            user.Work = data.Work;
+            await UserService.UpdateUserAsync(resultValidate.user.Id, user);
+            return Ok("User is Updated");
         }
         [Authorize]
         [HttpPost("SendMessage")]
         public async Task<ActionResult> SendMessage(string id, string text)
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
             User? recipient = await UserService.FindUserByIdAsync(id);
             if (recipient == null) return NotFound("Recipient not found!");
 
-            Message? message = await UserService.SendMessageAsync(resultValidate.userId, id, text);
+            Message? message = await UserService.SendMessageAsync(resultValidate.user.Id, id, text);
             if (message == null) return Conflict("Send message failed");
 
             return Ok("Ok");
@@ -171,37 +178,37 @@ namespace backend.Controllers
         [HttpGet("GetDialogs")]
         public async Task<ActionResult> GetDialogs()
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
-            var result = await UserService.GetDialogs(resultValidate.userId);
+            var result = await UserService.GetDialogs(resultValidate.user.Id);
             return Ok(result);
         }
         [Authorize]
         [HttpGet("GetMessages")]
         public async Task<ActionResult> GetMessages(string id)
         {
-            (string response, string userId) resultValidate = await ValidationUser();
-            if (resultValidate.response != "") return Unauthorized(resultValidate.response);
+            (string response, User? user) resultValidate = await ValidationUser();
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
 
             User? recipient = await UserService.FindUserByIdAsync(id);
             if (recipient == null) return NotFound("Recipient not found!");
 
-            var result = await UserService.GetMessages(resultValidate.userId, id);
+            var result = await UserService.GetMessages(resultValidate.user.Id, id);
             return Ok(result);
         }
 
-        private async Task<(string, string)> ValidationUser()
+        private async Task<(string, User?)> ValidationUser()
         {
             Claim? claimId = this.HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.PrimarySid);
-            if (claimId == null) return ("User not authorize!", "");
+            if (claimId == null) return ("User not authorize!", null);
 
             User? sender = await UserService.FindUserByIdAsync(claimId.Value);
-            if (sender == null) return ("Sender not found!", "");
+            if (sender == null) return ("Sender not found!", null);
 
             sender.LastVisit = DateTime.UtcNow;
             await UserService.UpdateUserAsync(claimId.Value, sender);
-            return ("", claimId.Value);
+            return ("", sender);
         }
     }
 }
