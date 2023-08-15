d0a068aa538935f13269a8dbb28c90e826b105e4
Illia <Illia@DESKTOP-TG4JUT1>
2023-08-15 14:30:55 +0200

    +add model

---
+++ backend/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Models
+{
+    public class Post
+    {
+        public string? Id { get; set; }
+        public string? UserId { get; set; }
+        public string? Text { get; set; }
+        public string? MediaUrl { get; set; }
+        public int LikesCount { get; set; }
+        public int RepostsCount { get; set; }
+        public List<Comment>? Comments { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
