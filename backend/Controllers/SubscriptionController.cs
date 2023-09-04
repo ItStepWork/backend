deb0bac77627778a8bb124b95bafc28b50f4408f
VLola <valiklolanew@gmail.com>
2023-09-04 19:35:08 +0300

    added SubscribeToMessagesUpdates

--- backend/Controllers/SubscriptionController.cs
+++ backend/Controllers/SubscriptionController.cs
@@ -108,5 +108,91 @@ namespace backend.Controllers
                 }
             }
         }
+        [Authorize]
+        [HttpGet("SubscribeToMessagesUpdates")]
+        public async Task SubscribeToMessagesUpdates()
+        {
+            var resultValidate = await UserService.ValidationUser(this.HttpContext);
+            if (resultValidate.user == null || resultValidate.user.Id == null)
+            {
+                HttpContext.Response.StatusCode = 400;
+            }
+            else
+            {
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync("client");
+                var startTime = DateTime.UtcNow;
+                var endTime = DateTime.UtcNow.AddMinutes(2);
+                _ = Task.Run(async () =>
+                {
+                    var buffer = new byte[1024];
+                    while (webSocket.State == WebSocketState.Open)
+                    {
+                        var receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                        if (webSocket.State == WebSocketState.Open)
+                        {
+                            endTime = DateTime.UtcNow.AddMinutes(2);
+                        }
+                    }
+                });
+                List<string> messages = new();
+                DateTime start = DateTime.UtcNow;
+                FirebaseClient firebaseDatabase = new FirebaseClient("https://database-50f39-default-rtdb.europe-west1.firebasedatabase.app/");
+                firebaseDatabase.Child($"Messages/{resultValidate.user.Id}").AsObservable<object>().Subscribe( data =>
+                {
+                    if (DateTime.UtcNow < endTime)
+                    {
+                        if (DateTime.UtcNow > start.AddSeconds(5))
+                        {
+                            string json = JsonConvert.SerializeObject(data.Object);
+                            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, Message>>(json);
+                            if (dictionary != null)
+                            {
+                                var last = dictionary.LastOrDefault().Value;
+                                if (last.Id != null)
+                                {
+                                    if (!messages.Contains(last.Id))
+                                    {
+                                        messages.Add(last.Id);
+                                        if (!string.IsNullOrEmpty(last.Text) && !string.IsNullOrEmpty(last.SenderId) && last.SenderId != resultValidate.user.Id)
+                                        {
+                                            if (webSocket.State == WebSocketState.Open)
+                                            {
+                                                var serverMsg = Encoding.UTF8.GetBytes("Update");
+                                                webSocket.SendAsync(serverMsg, WebSocketMessageType.Text, true, CancellationToken.None);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            string json = JsonConvert.SerializeObject(data.Object);
+                            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, Message>>(json);
+                            if (dictionary != null)
+                            {
+                                var last = dictionary.LastOrDefault().Value;
+                                if (last.Id != null)
+                                {
+                                    if (!messages.Contains(last.Id))
+                                    {
+                                        messages.Add(last.Id);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else if (webSocket.State == WebSocketState.Open)
+                    {
+                        webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closed", CancellationToken.None);
+                    }
+                });
+                while (webSocket.State == WebSocketState.Open)
+                {
+                    await Task.Delay(1000);
+                }
+            }
+        }
     }
 }
