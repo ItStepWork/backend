026d528dd0a6b1d880e510e978b520c471b2dea2
VLola <valiklolanew@gmail.com>
2023-09-15 22:22:11 +0300

    added admin controller and service

---
+++ backend/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using backend.Models;
+using backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AdminController : Controller
+    {
+        [HttpGet("GetUsers")]
+        public async Task<ActionResult> GetUsers()
+        {
+            var resultValidate = await AdminService.ValidationAdmin(this.HttpContext);
+            if (resultValidate.user == null || resultValidate.user.Id == null) return Unauthorized(resultValidate.response);
+
+            var result = await UserService.GetUsersAsync();
+            return Ok(result);
+        }
+    }
+}
---
+++ backend/Services/AdminService.cs
@@ -0,0 +1,24 @@
+﻿using backend.Models;
+using System.Security.Claims;
+
+namespace backend.Services
+{
+    public static class AdminService
+    {
+        public static async Task<(string response, User? user)> ValidationAdmin(HttpContext httpContext)
+        {
+            Claim? claimRole = httpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role);
+            Claim? claimId = httpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.PrimarySid);
+            if (claimId == null || claimRole == null) return ("User not authorize!", null);
+
+            User? sender = await UserService.FindUserByIdAsync(claimId.Value);
+            if (sender == null) return ("Sender not found!", null);
+
+            if (sender.Role != Models.Enums.Role.Admin || sender.Role.ToString() != claimRole.Value) return ("User not access!", null);
+
+            sender.LastVisit = DateTime.UtcNow;
+            await UserService.UpdateUserAsync(claimId.Value, sender);
+            return ("", sender);
+        }
+    }
+}
